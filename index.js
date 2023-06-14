729bc222a909863c2e10339a699c08e7db06c809
Meet Rajput <rajputmeet10@gmail.com>
2023-06-14 12:03:31 +0530

    added support for movies and web series

--- index.js
+++ index.js
@@ -38,6 +38,37 @@ async function scrapeGoogleDrive(searchParam) {
     throw new Error('Failed to scrape Google search results');
   }
 }
+async function scrapeMovies(searchParam) {
+  const searchQuery = `"${searchParam}" -inurl:(htm|html|php|pls|txt) intitle:index.of “last modified” (mp4|wma|aac|avi)`;
+const url = `https://www.google.com/search?q=${encodeURIComponent(searchQuery)}`;
+console.log(searchQuery, url);
+try {
+  const response = await axios.get(url);
+  const $ = cheerio.load(response.data);
+  const results = [];
+  $('a').each((index, element) => {
+    const href = $(element).attr('href');
+    const title = $(element).find('h3').text();
+    console.log(href, title);
+    if (href && title && href.startsWith('/url?q=') && title.startsWith("Index of")) {
+      const url = href;
+      const startString = '/url?q=';
+      const endString = `&sa`;
+    
+      const startIndex = url.indexOf(startString) + startString.length;
+      const endIndex = url.indexOf(endString); // Updated line
+      const extractedString = url.substring(startIndex, endIndex);
+    
+      results.push({ extractedString, title });
+    }
+    
+  
+  });
+  return results;
+} catch (error) {
+  throw new Error('Failed to scrape Google search results');
+}
+}
 async function scrapeGoogle(searchParam, filetype) {
   const searchQuery = `"${searchParam}" filetype:${filetype}`;
 const url = `https://www.google.com/search?q=${encodeURIComponent(searchQuery)}`;
@@ -92,11 +123,28 @@ app.get('/googleDrive', function(req, res) {
       });
   });
 
+
+  //scrape files
   app.get('/files', function(req, res) {
     const searchQuery = req.query.q; // Get the search query from the URL query parameters
     const fileType = req.query.type; // Get the file type from the URL query parameters
   
     scrapeGoogle(searchQuery, fileType)
+      .then(results => {
+        console.log('Search Results:', results);
+        res.send(results); // Send the results in the response
+      })
+      .catch(error => {
+        console.error('Scraping Error:', error);
+        res.status(500).send('Error occurred during scraping.'); // Send an error response
+      });
+  });
+
+  //scrape movies
+  app.get('/media', function(req, res) {
+    const searchQuery = req.query.q; // Get the search query from the URL query parameters
+  
+    scrapeMovies(searchQuery)
       .then(results => {
         console.log('Search Results:', results);
         res.send(results); // Send the results in the response
